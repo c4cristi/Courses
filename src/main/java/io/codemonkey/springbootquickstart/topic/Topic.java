07f08e1fde9031cf041bef346bf66b2c4a8b6afd
cristiandobre666 <c4_cristi@yahoo.com>
2023-02-26 01:03:49 +0200

    Create standalone springboot application and a simple rest controller with 2 endpoints

---
+++ src/main/java/io/codemonkey/springbootquickstart/topic/Topic.java
@@ -0,0 +1,14 @@
+package io.codemonkey.springbootquickstart.topic;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Topic {
+    private String id;
+    private String name;
+    private String description;
+}
